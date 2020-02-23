9c5a04650592e8c17e653c0c4b2cf5239709a1de
Shane Hopcroft <shanehopcroft@hotmail.com>
2020-02-23 21:17:52 +1100

    Adding ethernet initialisation code

--- aardvark.ino
+++ aardvark.ino
@@ -51,6 +51,26 @@ void setup()
 	
 	// Analog pins
 	pinMode(userInputPotPin, INPUT);
+	// // Check for Ethernet hardware present
+	// if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+	// 	Serial.println("Ethernet shield was not found. Sorry, can't run without hardware. :(");
+	// 	while (true) {
+	// 		delay(1); // do nothing, no point running without Ethernet hardware
+	// 	}
+	// }
+
+	// while (Ethernet.linkStatus() == LinkOFF) {
+	// 	Serial.println("Ethernet cable is not connected, trying again...");
+	// 	delay(5000);
+	// }
+
+	while (Ethernet.begin(mac) != 1) {
+		Serial.println("Error getting IP address via DHCP, trying again...");
+		delay(5000);
+	}
+
+	Serial.print("DHCP assigned IP ");
+	Serial.println(Ethernet.localIP());
 }
 
 void loop()
@@ -69,6 +89,7 @@ void loop()
         digitalWrite(overThresholdLedPin, HIGH);
 
         snsPublish(SNS_TOPIC_ARN, "Arduino%20rules%21");
+		delay(10000);
     }
     else
     {
@@ -193,6 +214,8 @@ void snsPublish(const char* topic, const char* message)
 				Serial.print(c);
 			}
 		}
+
+		Serial.println("<End>");
 
 		// Close the connection after the server closes its end
 		client.stop();

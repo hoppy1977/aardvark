d63a82341ca5dc99458939fe2aad98b2e6b87a6e
Shane Hopcroft <shanehopcroft@hotmail.com>
2020-02-26 22:04:01 +1100

    Hacked sketch to prove http connectivity was working

--- aardvark.ino
+++ aardvark.ino
@@ -28,7 +28,7 @@ unsigned long currentTime = 1581834590; // TODO:
 #define     AWS_SECRET_ACCESS_KEY  "TODO:"						// Put your AWS secret access key here.
 #define     AWS_REGION             "ap-southeast-2"													// The region where your SNS topic lives.
 																									// See the table at: http://docs.aws.amazon.com/general/latest/gr/rande.html#sns_region
-#define     AWS_HOST               "sns.ap-southeast-2.amazonaws.com"								// The host URL for the region where your SNS topic lives.
+#define     AWS_HOST               "google.com"								// The host URL for the region where your SNS topic lives.
 																									// See the table at: http://docs.aws.amazon.com/general/latest/gr/rande.html#sns_region
 #define     SNS_TOPIC_ARN          "arn%3Aaws%3Asns%3Aap-southeast-2%3A244360212732%3Aaardvark-demo-MySNSTopic-EH981W003O13"	// Amazon resource name (ARN) for the SNS topic to receive notifications.
 																																// Note: This ARN _MUST_ be URL encoded!  See http://meyerweb.com/eric/tools/dencoder/ for an example URL encoder tool.
@@ -106,95 +106,15 @@ void loop()
 // Note, both the topic and message strings _MUST_ be URL encoded before calling this function!
 void snsPublish(const char* topic, const char* message)
 {
-    // Set dateTime to the URL encoded ISO8601 format string.
-	DateTime dt(currentTime);
-	char dateTime[25];
-	memset(dateTime, 0, 25);
-	dateTime8601UrlEncoded(dt.year(), dt.month(), dt.day(), dt.hour(), dt.minute(), dt.second(), dateTime);
-
-	// Generate the signature for the request.
-	// For details on the AWS signature process, see: 
-	//   http://docs.aws.amazon.com/general/latest/gr/signature-version-2.html
-	Sha1.initHmac((uint8_t*)AWS_SECRET_ACCESS_KEY, strlen(AWS_SECRET_ACCESS_KEY));
-	Sha1.print(F("POST\n"));
-	Sha1.print(AWS_HOST);
-	Sha1.print(F("\n"));
-	Sha1.print(F("/\n"));
-	Sha1.print(F("AWSAccessKeyId="));
-	Sha1.print(AWS_ACCESS_KEY);
-	Sha1.print(F("&Action=Publish"));
-	Sha1.print(F("&Message="));
-	Sha1.print(message);
-	Sha1.print(F("&SignatureMethod=HmacSHA1"));
-	Sha1.print(F("&SignatureVersion=2"));
-	Sha1.print(F("&Timestamp="));
-	Sha1.print(dateTime);
-	Sha1.print(F("&TopicArn="));
-	Sha1.print(topic);
-	Sha1.print(F("&Version=2010-03-31"));
-
-	// Convert signature to base64
-	// Adapted from Adafruit code for SendTweet example.
-	uint8_t *in, out, i, j;
-	char b64[27];
-	memset(b64, 0, sizeof(b64));
-	static const char PROGMEM b64chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-	for (in = Sha1.resultHmac(), out = 0; ; in += 3) { // octets to sextets
-		b64[out++] = in[0] >> 2;
-		b64[out++] = ((in[0] & 0x03) << 4) | (in[1] >> 4);
-		if (out >= 26) break;
-		b64[out++] = ((in[1] & 0x0f) << 2) | (in[2] >> 6);
-		b64[out++] = in[2] & 0x3f;
-	}
-	b64[out] = (in[1] & 0x0f) << 2;
-	// Remap sextets to base64 ASCII chars
-	for (i = 0; i <= out; i++) b64[i] = pgm_read_byte(&b64chars[(unsigned char)b64[i]]);
-
-	// URL encode base64 signature.  Note, this is not a general URL encoding routine!
-	char b64Encoded[100];
-	memset(b64Encoded, 0, sizeof(b64Encoded));
-	for (i = 0, j = 0; i <= out; i++) {
-		uint8_t ch = b64[i];
-		if (ch == '+') {
-			b64Encoded[j++] = '%';
-			b64Encoded[j++] = '2';
-			b64Encoded[j++] = 'B';
-		}
-		else if (ch == '/') {
-			b64Encoded[j++] = '%';
-			b64Encoded[j++] = '2';
-			b64Encoded[j++] = 'F';
-		}
-		else {
-			b64Encoded[j++] = ch;
-		}
-	}
-	b64Encoded[j++] = '%';
-	b64Encoded[j++] = '3';
-	b64Encoded[j++] = 'D';
-
-	// Make request to SNS API
+	// Make request
 	int connectionResult = client.connect(AWS_HOST, 80);
 	if (connectionResult == 1)
 	{
-		Serial.println("Connected to AWS...");
+		Serial.println("Connected to host...");
 
 		String text;
-		text += F("POST /?");
-		text += F("AWSAccessKeyId=");
-		text += AWS_ACCESS_KEY;
-		text += F("&Action=Publish");
-		text += F("&Message=");
-		text += message;
-		text += F("&SignatureMethod=HmacSHA1");
-		text += F("&SignatureVersion=2");
-		text += F("&Timestamp=");
-		text += dateTime;
-		text += F("&TopicArn=");
-		text += topic;
-		text += F("&Version=2010-03-31");
-		text += F("&Signature=");
-		text += b64Encoded;
+		text += F("GET ");
+		text += AWS_HOST;
 		text += F(" HTTP/1.1\r\nHost: ");
 		text += AWS_HOST;
 		text += F("\r\nConnection: close\r\n");
